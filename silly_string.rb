b00cf2f171c2624d1a37d7b95c619d4dc3c551e7
irhabano234 <irha.bano@gmail.com>
2018-06-21 15:35:15 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,15 +1,15 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+your_favorite_word = "no"
 
-puts "My favorite word is #{your_favorite_word}"
+puts "My favorite word is #{no}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
 favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
 
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+puts #{no.upcase}"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
@@ -17,7 +17,7 @@ phrase = "IM NOT SHOUTING"
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
-puts lowercased_phrase
+puts lowercased_phrase .
 
 # Challenge 4: Count how many letters are in that big_word
 
